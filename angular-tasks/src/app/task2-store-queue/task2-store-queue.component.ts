f3152d20acabd60a422bdd0dc3384d64b4310eae
Julia Lepner <ylepner@gmail.com>
2023-06-05 17:00:00 +0200

    feat: define checkout and add to queue

--- angular-tasks/src/app/task2-store-queue/task2-store-queue.component.ts
+++ angular-tasks/src/app/task2-store-queue/task2-store-queue.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
 
+interface Checkout {
+  id: number;
+  items: items
+}
+
+const checkouts: Checkout[] = [
+  {
+    id: 1,
+    items: [2, 5]
+  },
+  {
+    id: 2,
+    items: [4, 10, 23]
+  },
+  {
+    id: 3,
+    items: [10]
+  },
+  {
+    id: 4,
+    items: [1, 1, 2]
+  },
+  {
+    id: 5,
+    items: [1, 6, 30]
+  },
+]
+
+type items = number[]
+
 @Component({
   selector: 'app-task2-store-queue',
   templateUrl: './task2-store-queue.component.html',
   styleUrls: ['./task2-store-queue.component.scss']
 })
-export class Task2StoreQueueComponent {
 
+
+export class Task2StoreQueueComponent {
+  itemsNumber: number = 0;
+  checkouts: Checkout[] = checkouts
+  queue: number[] = []
+
+  constructor() {
+  }
+
+  addToQueue(line: Checkout) {
+    line.items.push(Number(this.itemsNumber))
+    this.itemsNumber = 0
+    return line
+  }
+
+  defineCheckout() {
+    // define where is min items
+    let minItems: number = +Infinity
+    let line: Checkout = { id: 0, items: [] }
+    for (let checkout of checkouts) {
+      const allItemsInQueue = (checkout.items.reduce((a, b) => a + b, 0))
+      if (allItemsInQueue < minItems) {
+        minItems = allItemsInQueue
+        line = checkout
+      }
+    }
+    this.addToQueue(line)
+    console.log(line)
+  }
 }
