b83ea1dc29f84f39d8d4c86c07ee1bdc0b1bc722
Julia Lepner <ylepner@gmail.com>
2023-06-06 13:49:27 +0200

    feat: add numbers

--- angular-tasks/src/app/task3-passcode/task3-passcode.component.ts
+++ angular-tasks/src/app/task3-passcode/task3-passcode.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./task3-passcode.component.scss']
 })
 export class Task3PasscodeComponent {
-
+  numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 }
