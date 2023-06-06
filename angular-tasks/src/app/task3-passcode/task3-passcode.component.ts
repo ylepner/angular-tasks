4908c7157e2f4c97acfda8fd124e35c30b585aa2
Julia Lepner <ylepner@gmail.com>
2023-06-06 15:41:07 +0200

    feat: compare passwords

--- angular-tasks/src/app/task3-passcode/task3-passcode.component.ts
+++ angular-tasks/src/app/task3-passcode/task3-passcode.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class Task3PasscodeComponent {
   numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  password: number[] = [7, 3, 1, 6]
+  pressedNumbers: number[] = []
+  asterisks: string[] = []
+
+
+  addPressedNumbers(number: number) {
+    this.pressedNumbers.push(number)
+    this.asterisks.push('*')
+    console.log(`arr length = ${this.pressedNumbers.length}`)
+    if (this.pressedNumbers.length === 4) {
+      if (String(this.pressedNumbers) === String(this.password)) {
+        this.clearData()
+        return true
+      } else {
+        this.clearData()
+        return false
+      }
+    } else {
+      return
+    }
+  }
+
+  clearData() {
+    this.pressedNumbers = []
+    this.asterisks = []
+  }
 }
