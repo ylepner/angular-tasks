0af649da7ad4de628ddba77421d9813b4712a99e
Julia Lepner <ylepner@gmail.com>
2023-06-06 17:03:53 +0200

    style: style elements

--- angular-tasks/src/app/task3-passcode/task3-passcode.component.ts
+++ angular-tasks/src/app/task3-passcode/task3-passcode.component.ts
@@ -6,15 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./task3-passcode.component.scss']
 })
 export class Task3PasscodeComponent {
-  numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  numbers: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
   password: number[] = [7, 3, 1, 6]
   pressedNumbers: number[] = []
   asterisks: string[] = []
 
 
   addPressedNumbers(number: number) {
-    this.pressedNumbers.push(number)
     this.asterisks.push('*')
+    this.pressedNumbers.push(number)
     console.log(`arr length = ${this.pressedNumbers.length}`)
     if (this.pressedNumbers.length === 4) {
       if (String(this.pressedNumbers) === String(this.password)) {
