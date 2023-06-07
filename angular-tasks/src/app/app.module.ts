1c5009cdfdefcd5926a514d755045774b5ed0771
Julia Lepner <ylepner@gmail.com>
2023-06-07 10:43:13 +0200

    style: style component

--- angular-tasks/src/app/app.module.ts
+++ angular-tasks/src/app/app.module.ts
@@ -8,20 +8,28 @@ import { Task1Component } from './task1/task1.component';
 import { Task2StoreQueueComponent } from './task2-store-queue/task2-store-queue.component';
 import { Task3PasscodeComponent } from './task3-passcode/task3-passcode.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     Task1Component,
     Task2StoreQueueComponent,
-    Task3PasscodeComponent
+    Task3PasscodeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
